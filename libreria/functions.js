3b4d4aae63f2bdd844ab90c890ed53776dedb3d2
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-02-24 16:44:36 -0500

    Delete functions.js

--- libreria/functions.js
+++
@@ -1,7 +0,0 @@
-exports.getGroupAdmins = (participantes) => {
-    var admins = []
-    for (let i of participantes) {
-        i.admin === "admin" ? admins.push(i.id) : ''
-    }
-    return admins
-}
