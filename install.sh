aca694adf3dbe1500b617c956966da6715139068
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-02-24 19:17:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/bash
 
 clear
-apt-get update
-apt-get upgrade
-pkg install yarn
-npm i typescript -g
-yarn
-tsc -p ./node_modules/@adiwajshing/baileys/
+npm i
 
 echo "Use : npm start para ejecutar al bot :D"
