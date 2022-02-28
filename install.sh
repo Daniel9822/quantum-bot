0e47b8c8555fab4282a5ec3c2b11f453b8c32fd4
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-02-28 06:17:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/bash
 
 clear
+pkg update && pkg upgrade
+apt install nodejs
+apt install ffmpeg
 pkg install yarn
 yarn
 
 echo ""
+echo ""
 echo "Use : npm start para ejecutar al bot :D"
+echo ""
 echo ""
 echo ""
